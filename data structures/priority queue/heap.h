92cc45251c931ced579b94c2bfd12ff09380ee50
Zach <theY4Kman@gmail.com>
2008-11-21 19:44:35 -0500

    - HeapPriorityQueue now stores values as well as priorities.  - Added GPLv3 preamble to linked_list.h

--- data structures/priority queue/heap.h
+++ data structures/priority queue/heap.h
@@ -1,8 +1,40 @@
+/**
+ * =============================================================================
+ * Priority Queue
+ *   An implementation of a priority queue using a heap structure. Holds a
+ *   priority and value for each item.
+ * Copyright (C) 2008 Zach "theY4Kman" Kanzler
+ * =============================================================================
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 3.0, as published by the
+ * Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstdio>
+
+template<class K>
 class HeapPriorityQueue
 {
 private:
+    struct node
+    {
+        /// Integer value of the node
+        int priority;
+        /// A value to store with the node
+        K* value;
+    };
+    
     /// Our heap/array.
-    int *m_pHeap;
+    node *m_pHeap;
     /// Length of our array/heap.
     unsigned int m_iMax;
     /// The largest used index in the heap at the moment
@@ -17,9 +49,9 @@ private:
         if(k > m_iN) return;
         
         unsigned int j = k;
-        int val = m_pHeap[j];
+        node val = m_pHeap[j];
         
-        while((k /= 2) > 0 && val > m_pHeap[k])
+        while((k /= 2) > 0 && val.priority > m_pHeap[k].priority)
         {
             m_pHeap[j] = m_pHeap[k];
             m_pHeap[k] = val;
@@ -36,7 +68,7 @@ private:
         if(k > m_iN) return;
         
         unsigned int j;
-        int val = m_pHeap[k];
+        node val = m_pHeap[k];
         
         unsigned int limit = m_iN / 2;
         
@@ -45,10 +77,10 @@ private:
             j = k + k;
             
             // Move to the right child if it is larger than the left child
-            if(j < m_iN && m_pHeap[j] < m_pHeap[j+1]) j++;
+            if(j < m_iN && m_pHeap[j].priority < m_pHeap[j+1].priority) j++;
             
             // If `val` is larger than both children of `k`, we're done.
-            if(val >= m_pHeap[j]) break;
+            if(val.priority >= m_pHeap[j].priority) break;
             
             m_pHeap[k] = m_pHeap[j];
             k = j;
@@ -63,7 +95,7 @@ public:
      */
     HeapPriorityQueue(unsigned int max) : m_iMax(max), m_iN(0)
     {
-        m_pHeap = new int[m_iMax];
+        m_pHeap = new node[m_iMax];
     };
     
     ~HeapPriorityQueue()
@@ -73,25 +105,30 @@ public:
     
     /**
      * Insert an item into the queue.
-     * @param   val: Item to add.
+     * @param   priority: Priority of the value to add.
+     * @param   val: Value to add.
      */
-    void insert(int val)
+    void insert(int priority, const K& val)
     {
         if(m_iN >= m_iMax-1) return;
         
-        m_pHeap[++m_iN] = val;
+        node item;
+        item.priority = priority;
+        item.value = (K*)&val;
+        
+        m_pHeap[++m_iN] = item;
         upheap(m_iN);
     }
     
     /**
      * Removes the largest item from the queue and returns it.
-     * @return: Largest item from the queue.
+     * @return: Value of the item with the highest priority.
      */
-    int remove()
+    K* remove()
     {
-        if(m_iN <= 1) return -1;
+        if(m_iN <= 1) return NULL;
         
-        int val = m_pHeap[1];
+        K *val = m_pHeap[1].value;
         m_pHeap[1] = m_pHeap[m_iN--];
         downheap(1);
         
