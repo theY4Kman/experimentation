fdf57c6a3eb0283ed3ff32b2d6fff99324543071
Zach "theY4Kman" Kanzler <they4kman@gmail.com>
2021-07-17 21:47:23 -0400

    ref(genexpr): use single, shared buf for all decodeToken.Chars in Decode
    This change came after realizing the total length of all decodeToken.Chars could never exceed ChromosomeSize.
    
    Using this single shared buf reduces the average allocations per iteration from around 1200-1500 to around 1010. I'd like to say average time per iteration has also improved a bit, but I did not record runtimes till after I started changing things =/

--- machine-learning/genetic-algorithms/genetic_expr.go
+++ machine-learning/genetic-algorithms/genetic_expr.go
@@ -803,7 +803,7 @@ returnDecodeResultCache:
 		evalErr:   d.evalErr,
 	})
 
-	return *d.evaluated, d.evalErr
+	goto returnDecodeResultCache
 }
 
 type Validity rune
@@ -823,10 +823,10 @@ const (
 )
 
 type decodeToken struct {
-	Type  decodeTokenType
-	Chars strings.Builder
-	Str   string
-	Indices []int
+	Type     decodeTokenType
+	Chars    []byte
+	CharsLen int
+	Indices  []int
 }
 
 func tokenTypeOfByte(c byte) decodeTokenType {
@@ -855,13 +855,17 @@ func (c *Chromosome) Decode() *DecodeResult {
 	exprBuf := c.ctx.decodeBufPool.Get().([]byte)
 	defer c.ctx.decodeBufPool.Put(exprBuf)
 
+	tokenCharsLen := 0
+	tokenCharsBuf := c.ctx.decodeBufPool.Get().([]byte)
+	defer c.ctx.decodeBufPool.Put(tokenCharsBuf)
+
 	writeByte := func(buf []byte, bufLen *int, c byte) {
 		buf[*bufLen] = c
 		*bufLen++
 	}
-	writeString := func(buf []byte, bufLen *int, str string) {
-		copy(buf[*bufLen:], str)
-		*bufLen += len(str)
+	writeBytes := func(buf []byte, bufLen *int, bytes []byte) {
+		copy(buf[*bufLen:], bytes)
+		*bufLen += len(bytes)
 	}
 
 	var tokens []decodeToken
@@ -878,15 +882,17 @@ func (c *Chromosome) Decode() *DecodeResult {
 				}
 
 				tokens = append(tokens, decodeToken{
-					Type:  toktype,
-					Chars: strings.Builder{},
-					Indices: make([]int, 0, indicesCap),
+					Type:     toktype,
+					Chars:    tokenCharsBuf[tokenCharsLen:],
+					CharsLen: 0,
+					Indices:  make([]int, 0, indicesCap),
 				})
 			}
 
 			token := &tokens[len(tokens)-1]
-			token.Chars.WriteByte(value)
-			token.Str = token.Chars.String()
+			token.Chars[token.CharsLen] = value
+			token.CharsLen++
+			tokenCharsLen++
 			token.Indices = append(token.Indices, i)
 
 			// NOTE: this validity may be reversed during parsing
@@ -900,9 +906,10 @@ func (c *Chromosome) Decode() *DecodeResult {
 
 	for i := range tokens {
 		tok := &tokens[i]
-		if len(tok.Str) == 0 {
+		if tok.CharsLen == 0 {
 			continue
 		}
+		tok.Chars = tok.Chars[:tok.CharsLen]
 
 		var peek *decodeToken = nil
 		var past *decodeToken = nil
@@ -917,25 +924,27 @@ func (c *Chromosome) Decode() *DecodeResult {
 		switch tok.Type {
 		case tokenTypeNumber:
 			// Remove leading zeroes
-			for len(tok.Str) > 1 && tok.Str[0] == '0' {
+			for len(tok.Chars) > 1 && tok.Chars[0] == '0' {
 				validityBuf[tok.Indices[0]] = byte(Invalid)
-				tok.Str = tok.Str[1:]
+				tok.Chars = tok.Chars[1:]
+				tok.CharsLen--
 				tok.Indices = tok.Indices[1:]
 			}
 
 			// Truncate to max digits
-			if len(tok.Str) > c.ctx.TermMaxDigits {
-				for k := c.ctx.TermMaxDigits; k < len(tok.Str); k++ {
+			if len(tok.Chars) > c.ctx.TermMaxDigits {
+				for k := c.ctx.TermMaxDigits; k < tok.CharsLen; k++ {
 					validityBuf[tok.Indices[k]] = byte(Invalid)
 				}
-				tok.Str = tok.Str[:c.ctx.TermMaxDigits]
+				tok.CharsLen = c.ctx.TermMaxDigits
+				tok.Chars = tok.Chars[:tok.CharsLen]
 			}
 
-			writeString(exprBuf, &exprLen, tok.Str)
+			writeBytes(exprBuf, &exprLen, tok.Chars[:tok.CharsLen])
 			validTokens = append(validTokens, tok)
 
 		case tokenTypeOperator:
-			op := tok.Str[0]
+			op := tok.Chars[0]
 
 			// Allow unary + or -, whenever followed by a number
 			isValidUnary := (op == '+' || op == '-') && (peek != nil && peek.Type == tokenTypeNumber)
